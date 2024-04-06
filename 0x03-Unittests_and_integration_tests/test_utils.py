04ec73c87d067171b6f2345a5ed98bf28333a8f4
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-06 09:39:14 +0000

    1. Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,11 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """method for testing the access map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """tests for incorrect inputs error"""
+        self.assertRaises(KeyError)
