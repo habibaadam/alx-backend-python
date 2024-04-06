334d25141c48ef5de51371012eaf72aa7af7eb7e
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-06 10:38:36 +0000

    4. Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
-"""Tests for client"""
-
+"""Test for Client
+"""
 
 import unittest
-from parameterized import parameterized
-from client import GithubOrgClient
 from unittest.mock import patch, PropertyMock
+from client import GithubOrgClient
+from parameterized import parameterized
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """class containing tests for github org client"""
-    @parameterized.expand([
-        ("google"),
-        ("abc")])
+    """This class contains tests for the
+    GithubOrgClient class
+    """
+    @parameterized.expand([("google"), ("abc")])
     @patch("client.get_json")
     def test_org(self, test_org_name, mock_get_json):
-        """tests whether githuborg client returns correct
-        output"""
-        mock_get_json.return_value = {"test_key": "test_org_name"}
+        """Test that GithubOrgClient.org returns
+        the correct value
+        """
+        mock_get_json.return_value = {"test_key": test_org_name}
         client = GithubOrgClient(test_org_name)
-
         self.assertEqual(client.org, {"test_key": test_org_name})
+
 
     def test_public_repos_url(self):
         """Test that the result of _public_repos_url
